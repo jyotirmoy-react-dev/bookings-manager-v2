240d2d4394dc066b21da25f7e05c05910a0ba60e
Jyotirmoy Nath <er.jyotirmoy.nath@gmail.com>
2018-05-07 20:45:08 +0530

    Second

--- src/app/managehotel/managehotel.component.ts
+++ src/app/managehotel/managehotel.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormGroup, FormControl, Validator} from '@angular/forms';
 @Component({
   selector: 'app-managehotel',
   templateUrl: './managehotel.component.html',
   styleUrls: ['./managehotel.component.css']
 })
 export class ManagehotelComponent implements OnInit {
-
+  hotelForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  createForm(){
+    this.hotelForm = new FormGroup({
+      
+    });
   }
 
 }
